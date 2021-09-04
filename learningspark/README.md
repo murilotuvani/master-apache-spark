d1a4a94af929159b842f684998dd5335af22b6e8
Murilo Tuvani <murilo.tuvani@gmail.com>
2021-09-04 10:42:12 -0300

    Primeiro projeto

---
+++ learningspark/README.md
@@ -0,0 +1,17 @@
+# Empresa do fornecedor do curso
+https://jobreadyprogrammer.com/
+
+# Repositorio com o exemplos do curso prontos
+https://github.com/imtiazahmad007/sparkwithjava
+
+# Iniciando o servidor com docker
+docker run -d --name spark <s>-p 5432:5432</s> -p 7077:7077 -e SPARK_MODE=master bitnami/spark</br>
+docker run -d --name spark -p 4040:4040 -p 7077:7077 -e SPARK_MODE=master bitnami/spark
+
+# Se quiser utilizar o PostreSQL
+docker run --name postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
+docker exec -it postgres /bin/bash
+create database spark_course_data;
+
+## Documentação da imagem do docker
+https://hub.docker.com/r/bitnami/spark
---
+++ learningspark/src/main/java/learningspark/Application.java
@@ -0,0 +1,62 @@
+package learningspark;
+
+import static org.apache.spark.sql.functions.concat;
+import static org.apache.spark.sql.functions.lit;
+
+import java.util.Properties;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
+
+public class Application {
+
+	public static void main(String args[]) throws InterruptedException {
+
+		// Create a session
+		SparkSession spark = new SparkSession.Builder().appName("CSV to DB").master("local").getOrCreate();
+
+		// get data
+		Dataset<Row> df = spark.read().format("csv").option("header", true)
+				.load("src/main/resources/name_and_comments.txt");
+
+		// Exibe o DataFrame com as tres primeiras linhas
+		df.show(3);
+
+		// Transformation
+		// concat devolve uma coluna concatenando outras
+		// cf.col() devolve o valor de outra coluna
+		// lit() devolve um literal como uma coluna
+		df = df.withColumn("full_name", concat(df.col("last_name"), lit(", "), df.col("first_name")));
+		
+		df.show();
+		
+		//criando um novo dataframe apenas para os registros
+		//onde a coluna "comment" tenha números (expressao regular \d+)
+		//este dataframe deve ser ordenado pela coluna 
+		df = df.filter(df.col("comment").rlike("\\d+")).orderBy(df.col("last_name").asc());
+		
+		df.show(3);
+
+		// Write to destination
+		if (false) {
+			String dbConnectionUrl = "jdbc:mysql://localhost/spark_course_data"; // <<- You need to create this database
+			Properties prop = new Properties();
+			prop.setProperty("driver", "com.mysql.cj.jdbc.Driver");
+			prop.setProperty("user", "root");
+			prop.setProperty("password", "root"); // <- The password you used while installing Postgres
+
+			df.write().mode(SaveMode.Overwrite).jdbc(dbConnectionUrl, "project1", prop);
+		} else {
+			String dbConnectionUrl = "jdbc:postgresql://localhost/spark_course_data"; // <<- You need to create this database
+			Properties prop = new Properties();
+			prop.setProperty("driver", "org.postgresql.Driver");
+			prop.setProperty("user", "postgres");
+			prop.setProperty("password", "postgres"); // <- The password you used while installing Postgres
+
+			df.write().mode(SaveMode.Overwrite).jdbc(dbConnectionUrl, "project1", prop);
+		}
+		
+	}
+}
